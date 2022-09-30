23b0902a4f46f2fa46456cea30bb5a778dfadb12
CHAMPION <champion.emilien.pro@gmail.com>
2022-09-30 21:31:03 +0200

    Added UI improvement to clear the main view

--- rickandmorty/ui/MainView.swift
+++ rickandmorty/ui/MainView.swift
@@ -10,33 +10,43 @@ import SwiftUI
 struct MainView: View {
     @ObservedObject var mainViewModel = MainViewModel()
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(mainViewModel.characterList, id: \.self) { character in
-                    NavigationLink {
-                        CharacterDetailUIView(characterId: character.id!)
-                    } label: {
-                        HStack {
-                            if #available(iOS 15.0, *) {
-                                AsyncImage(
-                                    url: URL(string: character.imageUrl ?? ""))
-                                { image in
-                                    image.resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .padding()
+        NavigationView {
+            GeometryReader { geo in
+                ScrollView {
+                    VStack(spacing: 8) {
+                        ForEach(mainViewModel.characterList, id: \.self) { character in
+                            NavigationLink {
+                                CharacterDetailUIView(characterId: character.id!)
+                            } label: {
+                                HStack {
+                                    if #available(iOS 15.0, *) {
+                                        AsyncImage(
+                                            url: URL(string: character.imageUrl ?? ""))
+                                        { image in
+                                            image.resizable()
+                                                .frame(width: geo.size.width / 2)
+                                                .aspectRatio(contentMode: .fit)
+                                                .cornerRadius(10)
+                                                .shadow(color: .black, radius: 2, x: 1, y: 1)
+                                        }
+                                    placeholder: {
+                                        ProgressView()
+                                    }
+                                    } else {
+                                        // Fallback on earlier versions
+                                    }
+                                    Spacer()
+                                    Text(character.name ?? "")
+                                        .foregroundColor(.black)
+                                    Spacer()
                                 }
-                            placeholder: {
-                                ProgressView()
                             }
-                            } else {
-                                // Fallback on earlier versions
-                            }
-                            Text(character.name ?? "")
-                            Spacer()
+                            .padding()
                         }
                     }
                 }
             }
+            .navigationTitle(Text("Liste des personnages"))
             .onAppear() {
                 Task {
                     await mainViewModel.fetchCharacterList()
